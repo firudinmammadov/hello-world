020e2f5c3f1413fe6c1ed5effbf98a2d1553aa7a
fullstack2201 <firudinmmmdov688@gmail.com>
2026-07-21 04:34:51 +0400

    Types

--- Main.java
+++ Main.java
@@ -2,6 +2,14 @@ package com.firudin;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+//        int age = 30;
+//        int temperature = 20;
+//        age = 35;
+//        System.out.println(age);
+
+//        int myAge = 30;
+//        int herAge = myAge;
+//        System.out.println(herAge);
     }
 }
