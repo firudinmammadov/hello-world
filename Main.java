2cd219b1ca3382ef962cd22d5d545833bf4c42c9
fullstack2201 <firudinmmmdov688@gmail.com>
2026-07-21 14:21:18 +0400

    Primitive Types vs Reference Types

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
 package com.firudin;
 
-import java.util.Date;
+
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        byte age = 30;
-        Date now = new Date();
-        System.out.println(now);
+//        byte x = 1;
+//        byte y = x;
+//        x = 2;
+//        System.out.println(y);
+
+//        Point point1 = new Point(1, 1);
+//        Point point2 = point1;
+//        point1.x = 2;
+//        System.out.println(point2);
+
     }
 }
