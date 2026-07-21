59fd8790cf1785c752fcd9dfe1f008df13814e11
fullstack2201 <firudinmmmdov688@gmail.com>
2026-07-22 03:27:54 +0400

    Escape sequences

--- Main.java
+++ Main.java
@@ -5,17 +5,17 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        String message = "   Hello World" + "!!";
-        System.out.println(message.endsWith("!!"));
-        System.out.println(message.startsWith("!!"));
-        System.out.println(message.length());
-        System.out.println(message.indexOf("H"));
-        System.out.println(message.indexOf("e"));
-        System.out.println(message.indexOf("sky"));
-        System.out.println(message.replace("!", "*"));
-        System.out.println(message.toLowerCase());
-        System.out.println(message.toUpperCase());
-        System.out.println(message.trim());
+//        String message = "Hello \"Firudin\"";
+//        System.out.println(message);
+
+//        String message = "c:\\Windows:\\";
+//        System.out.println(message);
+
+//        String message = "c:\nWindows:\\";
+//        System.out.println(message);
+
+        String message = "c:\tWindows:\\";
         System.out.println(message);
+
     }
 }
