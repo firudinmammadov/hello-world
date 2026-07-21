aff6f932eba57b55908d44cdd48fc9ff3e94c5c7
fullstack2201 <firudinmmmdov688@gmail.com>
2026-07-21 14:02:36 +0400

    Variables And Primitive Types

--- Main.java
+++ Main.java
@@ -2,14 +2,10 @@ package com.firudin;
 
 public class Main {
     public static void main(String[] args) {
-
-//        int age = 30;
-//        int temperature = 20;
-//        age = 35;
-//        System.out.println(age);
-
-//        int myAge = 30;
-//        int herAge = myAge;
-//        System.out.println(herAge);
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
     }
 }
