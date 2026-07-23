ee570f57ee7949e53eb7954914a4b505760eb5d9
fullstack2201 <firudinmmmdov688@gmail.com>
2026-07-23 10:21:31 +0400

    Arrays

--- Main.java
+++ Main.java
@@ -2,20 +2,13 @@ package com.firudin;
 
 
 import java.awt.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-//        String message = "Hello \"Firudin\"";
-//        System.out.println(message);
-
-//        String message = "c:\\Windows:\\";
-//        System.out.println(message);
-
-//        String message = "c:\nWindows:\\";
-//        System.out.println(message);
-
-        String message = "c:\tWindows:\\";
-        System.out.println(message);
+        int[] numbers = {2, 3, 5, 1, 4};
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
 
     }
 }
